4c7b98c1d23fbf8a6a364950976179fe857b8d8a
Sheikh Jabed <jabedbd2295@gmail.com>
2025-06-07 20:32:30 +0600

    Necessary installation and components setup done

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      taint: true,
-      serverExternalPackages: ["@next/swc-linux-x64-gnu"],
-    },
-    output: "standalone",
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    images: {
-      domains: ["res.cloudinary.com"],
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  experimental: {
+    taint: true,
+    serverExternalPackages: ["@next/swc-linux-x64-gnu"],
+  },
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
