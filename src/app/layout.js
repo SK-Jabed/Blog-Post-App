efeb604b998c5e4046fd5331fc0d0221cfaf165b
Sheikh Jabed <jabedbd2295@gmail.com>
2025-06-13 23:31:39 +0600

    [UPDATED]: Banner and Header section's activities updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import { dark } from "@clerk/themes";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import Header from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -18,12 +16,32 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    // <ClerkProvider
+    //   appearance={{
+    //     baseTheme: dark,
+    //   }}
+    // >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* Header */}
+            <Header />
+            {/* Main Content */}
+            <main className="min-h-screen">{children}</main>
+            {/* Footer */}
+            <footer className="bg-muted/50 py-14">
+              <div className="container mx-auto px-6 text-center text-gray-200">
+                <p>Created By Sheikh Jabed</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    // </ClerkProvider>
   );
 }
